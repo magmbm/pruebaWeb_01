ebb3c29bc4131e2f927e79a4970956045b3000fd
Plaza Vespucio <lop@gmail.com>
2024-04-12 14:07:08 -0400

    Segunda y final version

--- valid.js
+++ valid.js
@@ -1,12 +1,5 @@
 function submit1(){
     let text= 'Mensaje enviado con exito, le contactaremos pronto';
-    let text2= 'Error, la contraseña o correo no es valido';
-    if(document.getElementById("exampleInputEmail1").ariaValueMax.includes('@') &&
-        document.getElementById("exampleInputPassword1").ariaValueMax.length>0
-        && document.getElementById("exampleMotiv").ariaValueMax.length>0){
-        alert(text);
-    }else{
-        alert(text2);
-    }
+    alert(text);
 
 };
